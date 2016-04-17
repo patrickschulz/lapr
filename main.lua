fd27eb3d6b74b944eab5517dcb6c856300ea48da
patrickschulz <pschulz@posteo.de>
2016-04-17 13:32:02 +0200

    Edited main.lua according to changes in session.lua.

--- main.lua
+++ main.lua
@@ -3,11 +3,14 @@ local session_lib = require "session"
 
 local session = session_lib.create(session_lib.default_action_handlers)
 
+local pretty = require "pl.pretty"
+
 --if arg[1] == "-d" or arg[1] == "--debug" then
     print("starting debug mode")
     session:set_debug_mode("all")
 --end
 
+--[[
 session:add_action_handlers({
     { command = "create", action = project_lib.create, help_message = "create a project", save_data = "projectlib" },
     { command = "load", action = project_lib.load, help_message = "load a project", save_data = "projectlib" },
@@ -21,8 +24,31 @@ session:add_action_handlers({
     { command = "compile", action = project_lib.compile, help_message = "compile the document", use_data = "projectlib" },
     { command = "cleanup", action = project_lib.clean_up, help_message = "do a clean up: (re)move all files that don't belong to the project. Use 'add_aux_file' to protect files.", use_data = "projectlib" },
 })
+--]]
 
-session:add_options_map_to_handler("help", { foo = "bar" })
+session:add_action_handler({
+    command = "add",
+    action = {
+        content = project_lib.add_file,
+        aux     = project_lib.add_aux_file,
+        package = project_lib.add_package
+    },
+    help_message = "add is a super command. List of subcommands:\n  content: add a content file\n  aux: add an auxiliary file\n  package: add a package to the preamble",
+    use_data = "project_lib"
+})
+session:add_action_handler({
+    command = "edit",
+    action = project_lib.edit_file,
+    help_message = "edit a file",
+    use_data = "project_lib"
+})
+
+session:add_action_handler({
+    command = "create",
+    action = project_lib.create,
+    help_message = "create a project",
+    save_data = "project_lib"
+})
 
 session:loop()
 
